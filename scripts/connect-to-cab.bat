8ae8cde5a9c05888ff70cb342d4e8b648752dfbb
TheCodeAlwaysWins <thecodealwayswins@gmail.com>
2021-09-06 20:00:07 -0400

    connect-to-cab.bat: instead of exiting cmd, give a useful message when cab not connected

--- scripts/connect-to-cab.bat
+++ scripts/connect-to-cab.bat
@@ -2,8 +2,10 @@
 
 (adb devices | findstr "0123456789") >nul 2>&1
 
-if %errorlevel% neq 0 (
-	exit
+if %errorlevel% == 0 (
+	CALL adb-wifi.bat
+) else (
+	echo No cab connection found. 
+	echo Try unplugging/replugging in cab and waiting 4-5 seconds,
+	echo and be sure that you have installed the necessary drivers
 )
-
-CALL adb-wifi.bat
